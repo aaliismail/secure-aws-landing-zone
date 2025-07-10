7dc057a269180f7229ffa04989ad8e29b9818ca3
Ali Ismail <ismailali12065@gmail.com>
2025-07-10 14:08:28 -0500

    feat: Add networking layer with subnets and IGW

--- main.tf
+++ main.tf
@@ -27,3 +27,64 @@ resource "aws_vpc" "main" {
 
 
 
+
+resource "aws_subnet" "public" {
+
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public-subnet-1"
+    }
+}
+
+resource "aws_subnet" "private" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1b"
+
+
+    tags = {
+        Name = "private-subnet-1"
+    }
+}
+
+
+
+
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "main-igw"
+
+    }
+}
+
+
+
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+
+    tags = {
+        Name = "public-route-table"
+    }
+}
+
+
+resource "aws_route_table_association" "public" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public.id
+}
