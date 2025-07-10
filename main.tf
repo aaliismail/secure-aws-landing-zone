3ae8d7eab9ae0deb8f70d02c69072f3b32c8fc80
Ali Ismail <ismailali12065@gmail.com>
2025-07-10 14:32:57 -0500

    feat: Add web and database security groups

--- main.tf
+++ main.tf
@@ -88,3 +88,71 @@ resource "aws_route_table_association" "public" {
     subnet_id = aws_subnet.public.id
     route_table_id = aws_route_table.public.id
 }
+
+
+
+resource "aws_security_group" "web" {
+    name = "web-sg"
+    description = "Allow TLS/HTTP inbound traffic"
+    vpc_id = aws_vpc.main.id
+
+    ingress{
+        description = "HTTPS from anywhere"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    ingress {
+        description = "HTTP from anywhere"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks =["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks =["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "web-sg"
+
+    }
+}
+
+
+resource "aws_security_group" "db" {
+    name = "db-sg"
+    description = "Allow traffic from the web security group"
+    vpc_id = aws_vpc.main.id 
+
+
+    ingress {
+        description = "MySQL from web-sg"
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        security_groups = [aws_security_group.web.id]
+    }
+
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    tags = {
+        Name = "db-sg"
+    }
+}
+
